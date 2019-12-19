b84ec5a972f1b4ed3ad7c047b97f80d214687bfd
Eliot Jordan <eliotj@princeton.edu>
2019-12-19 11:35:13 -0600

    Initial work

---
+++ app/javascript/components/ajax-select.vue
@@ -0,0 +1,62 @@
+<template>
+  <v-select label="id" :value="selected" @input=byebug :filterable="false" :options="options" @search="onSearch">
+    <template slot="no-options">
+      type to search for numismatic places...
+    </template>
+    <template slot="option" slot-scope="option">
+      <div class="d-center">
+        {{ option["figgy_title_ssi"] }}
+        </div>
+    </template>
+    <template slot="selected-option" slot-scope="option">
+      <div class="selected d-center">
+        {{ option["figgy_title_ssi"] }}
+      </div>
+    </template>
+  </v-select>
+</template>
+<script>
+import vSelect from 'vue-select'
+import _ from 'lodash'
+export default {
+  name: 'AjaxSelect',
+  components: {
+    'v-select': vSelect
+  },
+  props: {
+    typeName: {
+      type: String,
+      required: true
+    }
+  },
+  data () {
+    return {
+      options: [],
+      selected: null
+    }
+  },
+  methods: {
+    byebug (value) {
+      debugger  // eslint-disable-line
+      value = value || { id: null }
+      $("#numismatics_coin_find_place_id").val(value.id)
+      this.selected = value
+    },
+    searchURL (search) {
+      return `http://localhost:3000/catalog.json/?f%5Bhuman_readable_type_ssim%5D%5B%5D=${this.typeName}&all_models=true&q=${escape(search)}`
+    },
+    onSearch (search, loading) {
+      loading(true)
+      this.search(loading, search, this)
+    },
+    search: _.debounce((loading, search, vm) => {
+      fetch(
+        vm.searchURL(search)
+      ).then(res => {
+        res.json().then(json => (vm.options = json.response.docs))
+        loading(false)
+      })
+    }, 350)
+  }
+}
+</script>
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ import IssueMonograms from '../components/issue_monograms'
 import axios from 'axios'
 import OrderManager from '../components/OrderManager.vue'
 import setupAuthLinkClipboard from '../packs/auth_link_clipboard.js'
+import AjaxSelect from '../components/ajax-select'
 
 Vue.use(system)
 
@@ -17,7 +18,7 @@ document.addEventListener('DOMContentLoaded', () => {
   // Set CSRF token for axios requests.
   axios.defaults.headers.common['X-CSRF-Token'] = document.querySelector('meta[name="csrf-token"]').getAttribute('content')
   var elements = document.getElementsByClassName('lux')
-  for(var i = 0; i < elements.length; i++){
+  for (var i = 0; i < elements.length; i++) {
     new Vue({
       el: elements[i],
       store,
@@ -25,7 +26,25 @@ document.addEventListener('DOMContentLoaded', () => {
         'document-adder': DocumentAdder,
         'playlistMembers': PlaylistMembers,
         'order-manager': OrderManager,
-        'issue-monograms': IssueMonograms
+        'issue-monograms': IssueMonograms,
+        'ajax-select': AjaxSelect
+      },
+      data: {
+        options: []
+      },
+      methods: {
+        onSearch(search, loading) {
+          loading(true);
+          this.search(loading, search, this);
+        },
+        search: _.debounce((loading, search, vm) => {
+          fetch(
+            `http://localhost:3000/catalog.json/?f%5Bhuman_readable_type_ssim%5D%5B%5D=Issue&q=figgy_title_ssi:*${escape(search)}`
+          ).then(res => {
+            res.json().then(json => (vm.options = json.response.docs));
+            loading(false);
+          });
+        }, 350)
       }
     })
   }
