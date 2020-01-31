137a48577583da4309c9b74dc0359c5202c7c6db
Trey Pendragon <tpendragon@princeton.edu>
2020-01-31 15:52:08 -0800

    More Lando fiddling around.

--- config/initializers/figgy.rb
+++ config/initializers/figgy.rb
@@ -4,6 +4,57 @@ module Figgy
     @config ||= config_yaml.with_indifferent_access
   end
 
+  class Lando
+    def database
+      return default_database unless status
+      db_status = status["figgy_#{Rails.env.downcase}_db"][0]
+      {
+        host: db_status["external_connection"]["host"],
+        port: db_status["external_connection"]["port"],
+        username: db_status["creds"]["user"],
+        password: db_status["creds"]["password"]
+      }
+    end
+
+    def solr
+      return default_solr unless status
+      solr_status = status["figgy_#{Rails.env.downcase}_solr"][0]
+      {
+        host: solr_status["external_connection"]["host"],
+        port: solr_status["external_connection"]["port"]
+      }
+    end
+
+    def default_database
+      {
+        host: nil,
+        port: nil,
+        username: nil,
+        password: nil
+      }
+    end
+
+    def default_solr
+      {
+        host: "localhost",
+        port: "8983"
+      }
+    end
+
+    def status
+      @status ||=
+        begin
+          JSON.parse(`lando info --format json`).group_by { |x| x["service"] }
+        rescue
+          nil
+        end
+    end
+  end
+
+  def lando_config
+    @lando_config ||= Lando.new
+  end
+
   def messaging_client
     @messaging_client ||= MessagingClient.new(Figgy.config["events"]["server"])
   end
@@ -34,5 +85,5 @@ module Figgy
       YAML.safe_load(ERB.new(File.read(Rails.root.join("config", "config.yml"))).result, [], [], true)[Rails.env]
     end
 
-    module_function :config, :config_yaml, :messaging_client, :geoblacklight_messaging_client, :geoserver_messaging_client, :orangelight_messaging_client, :default_url_options, :campus_ip_ranges
+    module_function :config, :config_yaml, :messaging_client, :geoblacklight_messaging_client, :geoserver_messaging_client, :orangelight_messaging_client, :default_url_options, :campus_ip_ranges, :lando_config
 end
