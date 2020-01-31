8559215d3b7cd41f99ce28d21db6384dd839a793
Trey Pendragon <tpendragon@princeton.edu>
2020-01-31 15:54:15 -0800

    Better clean solr.

--- spec/support/clean_solr.rb
+++ spec/support/clean_solr.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
-SOLR_TEST_URL = "http://127.0.0.1:#{ENV['TEST_JETTY_PORT'] || 8984}/solr/figgy-core-test"
+SOLR_TEST_URL = "http://#{Figgy.lando_config.solr[:host]}:#{ENV['TEST_JETTY_PORT'] || Figgy.lando_config.solr[:port]}/solr/figgy-core-test"
 RSpec.configure do |config|
   config.before(:each) do
     client = RSolr.connect(url: SOLR_TEST_URL)
